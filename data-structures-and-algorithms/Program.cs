43c925a31999ff7f6b4ff4d880177c2e1a9d881d
NTC <NTC@DESKTOP-83QUP11>
2023-06-16 20:41:53 +0300

    Add project files.

---
+++ data-structures-and-algorithms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace data_structures_and_algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
