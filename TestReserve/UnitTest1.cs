45a6cc2b93ebaafa8bd8a00c4a17f4f6c60ba273
osamaalkarmi20 <osamaalkarmi20.outlook@outlook.com>
2023-06-30 23:40:31 +0300

    linkedlist inser

--- TestReserve/UnitTest1.cs
+++ TestReserve/UnitTest1.cs
@@ -2,23 +2,22 @@ using data_structures_and_algorithms;
 
 namespace TestReserve
 {
-  
+   
         public class UnitTest1
         {
-
             [Fact]
             public void EmptyLinkedList()
             {
                 LinkedList linkedList = new LinkedList();
-                Assert.Equal(null,linkedList.Head);
+                Assert.Equal(null, linkedList.Head);
             }
 
             [Fact]
-            public void Insert()
+            public void Append()
             {
                 LinkedList linkedList = new LinkedList();
-                linkedList.Insert(5);
-       
+                linkedList.Append(5);
+
                 Assert.Equal(5, linkedList.Head.Value);
             }
 
@@ -26,56 +25,89 @@ namespace TestReserve
             public void Head()
             {
                 LinkedList linkedList = new LinkedList();
-                linkedList.Insert(5);
-                linkedList.Insert(10);
+                linkedList.Append(5);
+                linkedList.Append(10);
                 Assert.Equal(5, linkedList.Head.Value);
             }
 
             [Fact]
-            public void InsertMultiple()
+            public void AppendMultiple()
             {
                 LinkedList linkedList = new LinkedList();
-                linkedList.Insert(5);
-                linkedList.Insert(10);
-                linkedList.Insert(15);
+                linkedList.Append(5);
+                linkedList.Append(10);
+                linkedList.Append(15);
                 Assert.NotNull(linkedList.Tail);
                 Assert.Equal(15, linkedList.Tail.Value);
             }
 
-            [Fact]
-            public void IncludeTrue()
-            {
-                LinkedList linkedList = new LinkedList();
-                linkedList.Insert(5);
-                linkedList.Insert(10);
-                linkedList.Insert(15);
-                bool exists = linkedList.Includes(10);
-                Assert.Equal(true,exists);
-            }
-
-            [Fact]
-            public void IncludeFalse()
-            {
-                LinkedList linkedList = new LinkedList();
-                linkedList.Insert(5);
-                linkedList.Insert(10);
-                linkedList.Insert(15);
-                bool exists = linkedList.Includes(20);
-                Assert.Equal(false,exists);
-            }
+         
 
             [Fact]
             public void AllLinkedList()
             {
                 LinkedList linkedList = new LinkedList();
-                linkedList.Insert(5);
-                linkedList.Insert(10);
-                linkedList.Insert(15);
+                linkedList.Append(5);
+                linkedList.Append(10);
+                linkedList.Append(15);
                 string expected = "{ 5 } -> { 10 } -> { 15 } -> NULL";
                 string result = linkedList.ToString();
                 Assert.Equal(expected, result);
             }
+        [Fact]
+        public void BeforeMiddle()
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Append(5);
+            linkedList.Append(10);
+            linkedList.Append(15);
+            linkedList.InsertBefore(10, 7);
+            string expected = "{ 5 } -> { 7 } -> { 10 } -> { 15 } -> NULL";
+            string result = linkedList.ToString();
+            Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void BeforeFirst()
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Append(5);
+            linkedList.Append(10);
+            linkedList.InsertBefore(5, 2);
+            string expected = "{ 2 } -> { 5 } -> { 10 } -> NULL";
+            string result = linkedList.ToString();
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void AfterMiddle()
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Append(5);
+            linkedList.Append(10);
+            linkedList.Append(15);
+            linkedList.InsertAfter(10, 12);
+            string expected = "{ 5 } -> { 10 } -> { 12 } -> { 15 } -> NULL";
+            string result = linkedList.ToString();
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void AfterLast()
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Append(5);
+            linkedList.Append(10);
+            linkedList.InsertAfter(10, 13);
+            string expected = "{ 5 } -> { 10 } -> { 13 } -> NULL";
+            string result = linkedList.ToString();
+            Assert.Equal(expected, result);
+        }
+
     }
+}
+
+
+    
 
 
---
+++ data-structures-and-algorithms/Linked-list-insartions/README.md
@@ -0,0 +1,5 @@
+this is a simple way to make a single link list ,the code provides basic operations for inserting nodes, searching for values, inserting before or after a certain node and generating a string representation of a singly linked list. 
+this is the whiteboard for the problem :
+
+![whitebourd](link.png)
+![whitebourd](link.png)
