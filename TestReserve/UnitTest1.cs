f62c826a0c3e8b37df89a5f03b49c0431ca753d5
osamaalkarmi20 <osamaalkarmi20@DESKTOP-00FVEU4>
2023-08-05 21:53:13 +0300

    done

--- TestReserve/UnitTest1.cs
+++ TestReserve/UnitTest1.cs
@@ -1,88 +1,37 @@
-using data_structures_and_algorithms;
-using System.Collections;
-using System.Collections.Generic;
 using static data_structures_and_algorithms.Program;
-using static data_structures_and_algorithms.Program.BinaryTree;
+
 
 namespace TestReserve
 {
 
     public class UnitTest1
     {
-       
+
         public class BinarySearchTreeTests
         {
-            [Fact]
-            public void TestEmptyTree()
-            {
-                BinarySearchTree testTree = new BinarySearchTree();
-                Assert.False(testTree.Contains(5));
-            }
-            private BinarySearchTree CreateBinarySearchTree()
-            {
-                BinarySearchTree testTree = new BinarySearchTree();
-                testTree.Add(100);
-                testTree.Add(50);
-                testTree.Add(200);
-                testTree.Add(25);
-                testTree.Add(75);
-                testTree.Add(150);
-                testTree.Add(300);
-                testTree.Add(60);
-                testTree.Add(90);
-                testTree.Add(342);
-                return testTree;
-            }
 
             [Fact]
-            public void HappyPath()
+            public void BFStest()
             {
-               
-               BinarySearchTree testTree = CreateBinarySearchTree();
 
-                int result = testTree.Max_tree();
 
-                
-                Assert.Equal(342, result);
-            }
+                BinaryTree binaryTree = new BinaryTree();
+                binaryTree.Add(2);
+                binaryTree.Add(7);
+                binaryTree.Add(5);
+                binaryTree.Add(2);
+                binaryTree.Add(6);
+                binaryTree.Add(9);
+                binaryTree.Add(5);
+                binaryTree.Add(11);
+                binaryTree.Add(4);
 
-            [Fact]
-            public void Max_tree_TreeIsEmpty()
-            {
-                
-                BinarySearchTree testTree = new BinarySearchTree();
+                List<int> result = binaryTree.BFS(binaryTree.Root);
 
-                int result = testTree.Max_tree();
 
-              
-                Assert.Equal(0, result);
-            }
-
-            [Fact]
-            public void Max_tree_WhenSingleNodeTree()
-            {
-               
-                BinarySearchTree testTree = new BinarySearchTree();
-                testTree.Add(42);
-
-                
-                int result = testTree.Max_tree();
-
-                
-                Assert.Equal(42, result);
+                List<int> expected = new List<int> { 2, 7, 5, 2, 6, 9, 5, 11, 4 };
+                Assert.Equal(expected, result);
             }
         }
     }
-        }
-   
-
-
-
-
-
-
-
-
-
-
-
+}
