b5067c6b6b8591d514175310714b888f909b4e18
osamaalkarmi20 <osamaalkarmi20@DESKTOP-00FVEU4>
2023-08-30 00:08:26 +0300

    commit

--- TestReserve/UnitTest1.cs
+++ TestReserve/UnitTest1.cs
@@ -1,3 +1,4 @@
+using data_structures_and_algorithms;
 using static data_structures_and_algorithms.Program;
 
 
@@ -7,33 +8,15 @@ namespace TestReserve
     public class UnitTest1
     {
 
-        public class BinarySearchTreeTests
+        [Theory]
+        [InlineData(new[] { 8, 4, 23, 42, 16, 15 }, new[] { 4, 8, 15, 16, 23, 42 })]
+        [InlineData(new[] { 3, 1, 7, 2, 5 }, new[] { 1, 2, 3, 5, 7 })]
+        [InlineData(new[] { 10, 9, 8, 7, 6, 5 }, new[] { 5, 6, 7, 8, 9, 10 })]
+        
+        public void InsertionSort(int[] arr, int[] expected)
         {
-
-            [Fact]
-            public void FizzBuzzTreetest()
-            {
-
-
-                BinaryTree binaryTree = new BinaryTree();
-                binaryTree.Add("2");
-                binaryTree.Add("30");
-                binaryTree.Add("5");
-                binaryTree.Add("2");
-                binaryTree.Add("6");
-                binaryTree.Add("45");
-                binaryTree.Add("5");
-                binaryTree.Add("15");
-                binaryTree.Add("1");
-
-
-                binaryTree.FizzBuzzTree();
-                List<string> result = binaryTree.BFS(binaryTree.Root);
-
-                List<string> expected = new List<string>
-                { "2", "FizzBuzz", "Buzz", "2" ,"Fizz" , "FizzBuzz", "Buzz", "FizzBuzz", "1"};
-                Assert.Equal(expected, result);
-            }
+            int[] result = Program.InsertionSort(arr);
+            Assert.Equal(expected, result);
         }
     }
 }
--- data-structures-and-algorithms/Program.cs
+++ data-structures-and-algorithms/Program.cs
@@ -7,155 +7,36 @@ namespace data_structures_and_algorithms
     {
         static void Main(string[] args)
         {
-            BinaryTree binaryTree = new BinaryTree();
-            binaryTree.Add("2");
-            binaryTree.Add("7");
-            binaryTree.Add("5");
-            binaryTree.Add("2");
-            binaryTree.Add("6");
-            binaryTree.Add("9");
-            binaryTree.Add("5");
-            binaryTree.Add("15");
-            binaryTree.Add("4");
-
-            Console.WriteLine("Breadth-First Traversal: " + string.Join(" ", binaryTree.BFS(binaryTree.Root)));
-
-            binaryTree.FizzBuzzTree();
-
-            Console.WriteLine("FizzBuzz Tree Breadth-First Traversal: " + string.Join(" ", binaryTree.BFS(binaryTree.Root)));
+           
+            int[] arr = { 8, 4, 23, 42, 16, 15 };
+            int[] arrsorted = InsertionSort(arr);
+            for (int i = 0; i < arrsorted.Length; i++) {
+                Console.WriteLine(arrsorted[i]);
+            
+            }
         }
 
-        public class Node
+        static public int[] InsertionSort(int[] arr)
         {
-            public string Value { get; set; }
-            public Node Left { get; set; }
-            public Node Right { get; set; }
+            int key, j;
+            int n = arr.Length;
 
-            public Node(string value)
+            for (int i = 1; i < n; i++)
             {
-                Value = value;
-                Left = null;
-                Right = null;
+                key = arr[i];
+                j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = key;
             }
+
+            return arr;
         }
 
-        public class BinaryTree
-        {
-            public Node Root;
-
-            public BinaryTree()
-            {
-                Root = null;
-            }
-
-            public static Node CheckForNode(Node node)
-            {
-                if (Convert.ToInt32(node.Value) % 15 == 0)
-                {
-                    Node n1 = new Node("FizzBuzz");
-                    n1.Left = node.Left;
-                    n1.Right = node.Right;
-                    return n1;
-                }
-                else if (Convert.ToInt32(node.Value) % 5 == 0)
-                {
-                    Node n1 = new Node("Buzz");
-                    n1.Left = node.Left;
-                    n1.Right = node.Right;
-                    return n1;
-                }
-                else if (Convert.ToInt32(node.Value) % 3 == 0)
-                {
-                    Node n1 = new Node("Fizz");
-                    n1.Left = node.Left;
-                    n1.Right = node.Right;
-                    return n1;
-                }
-
-                return node;
-            }
-
-            public void FizzBuzzTree()
-            {
-                FizzBuzzTree(Root);
-            }
-
-            private void FizzBuzzTree(Node node)
-            {
-                if (node != null)
-                {
-                    FizzBuzzTree(node.Left);
-                    node.Value = CheckForNode(node).Value;
-                    FizzBuzzTree(node.Right);
-                }
-            }
-
-            public List<string> InorderTraversal()
-            {
-                List<string> result = new List<string>();
-                InorderTraversal(Root, result);
-                return result;
-            }
-
-            private void InorderTraversal(Node node, List<string> result)
-            {
-                if (node != null)
-                {
-                    InorderTraversal(node.Left, result);
-                    result.Add(node.Value);
-                    InorderTraversal(node.Right, result);
-                }
-            }
-
-            public List<string> BFS(Node root)
-            {
-                Queue<Node> queue = new Queue<Node>();
-                List<string> results = new List<string>();
-                if (root == null) return results;
-                queue.Enqueue(root);
-                while (queue.Count != 0)
-                {
-                    Node newNode = queue.Dequeue();
-                    results.Add(newNode.Value);
-                    if (newNode.Left != null)
-                    {
-                        queue.Enqueue(newNode.Left);
-                    }
-                    if (newNode.Right != null)
-                    {
-                        queue.Enqueue(newNode.Right);
-                    }
-                }
-                return results;
-            }
-
-            public void Add(string value)
-            {
-                Root = AddRecursive(Root, value);
-            }
-
-            private Node AddRecursive(Node current, string value)
-            {
-                if (current == null)
-                {
-                    return new Node(value);
-                }
-
-                if (current.Left == null)
-                {
-                    current.Left = new Node(value);
-                }
-                else if (current.Right == null)
-                {
-                    current.Right = new Node(value);
-                }
-                else
-                {
-                    current.Left = AddRecursive(current.Left, value);
-                }
-
-                return current;
-            }
-        }
     }
 }
