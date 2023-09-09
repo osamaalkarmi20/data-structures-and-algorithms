3d19b4e2d9824aa5c5f8f6701d1c6b733bb372e5
osamaalkarmi20 <osamaalkarmi20@DESKTOP-00FVEU4>
2023-09-09 13:48:05 +0300

    done

--- TestReserve/UnitTest1.cs
+++ TestReserve/UnitTest1.cs
@@ -1,4 +1,3 @@
-using data_structures_and_algorithms;
 using static data_structures_and_algorithms.Program;
 
 
@@ -10,36 +9,65 @@ namespace TestReserve
 
         public class Sort
         {
-
             [Fact]
-        public void TestMergeSort()
+            public void SortByName_ShouldSortMoviesByTitleIgnoringArticles()
             {
-                // Arrange
-                int[] arr = { 12, 11, 13, 5, 6, 7 };
-                int[] expectedSortedArr = { 5, 6, 7, 11, 12, 13 };
 
-                // Act
-                MergeSort(arr);
+                var movies = new List<Movie>
+            {
+                new Movie { Title = "The Lion King" },
+                new Movie { Title = "Toy Story" },
+                new Movie { Title = "Blade Runner" },
+                new Movie { Title = "Saving Private Ryan" },
+                new Movie { Title = "Scream" }
+            };
 
-                // Assert
-                Assert.Equal(expectedSortedArr, arr);
+
+                var sortedMovies = Sorting.SortByName(movies);
+
+                Assert.Equal("Blade Runner", sortedMovies[0].Title);
+                Assert.Equal("The Lion King", sortedMovies[1].Title);
+                Assert.Equal("Saving Private Ryan", sortedMovies[2].Title);
+                Assert.Equal("Scream", sortedMovies[3].Title);
+                Assert.Equal("Toy Story", sortedMovies[4].Title);
             }
 
             [Fact]
-            public void TestMerge()
+            public void CompareTitles_ShouldCompareTitlesIgnoringArticles()
             {
-                int[] left = { 5, 11, 13 };
-                int[] right = { 6, 7, 12 };
-                int[] expectedMergedArr = { 5, 6, 7, 11, 12, 13 };
-                int[] mergedArr = new int[left.Length + right.Length];
+                string title1 = "The Lion King";
+                string title2 = "Toy Story";
 
-                // Act
-           Merge(left, right, mergedArr);
 
-                // Assert
-                Assert.Equal(expectedMergedArr, mergedArr);
+                var result = Sorting.CompareTitles(title1, title2);
+
+
+                Assert.True(result < 0);
             }
 
+            [Fact]
+            public void SortByYear_ShouldSortMoviesByYear()
+            {
+
+                var movies = new List<Movie>
+            {
+                new Movie { Title = "The Lion King", Year = 1994 },
+                new Movie { Title = "Toy Story", Year = 1995 },
+                new Movie { Title = "Blade Runner", Year = 1982 },
+                new Movie { Title = "Saving Private Ryan", Year = 1998 },
+                new Movie { Title = "Scream", Year = 1996 }
+            };
+
+
+                var sortedMovies = Sorting.SortByYear(movies);
+
+
+                Assert.Equal(1982, sortedMovies[0].Year);
+                Assert.Equal(1994, sortedMovies[1].Year);
+                Assert.Equal(1995, sortedMovies[2].Year);
+                Assert.Equal(1996, sortedMovies[3].Year);
+                Assert.Equal(1998, sortedMovies[4].Year);
+            }
         }
     }
 }
